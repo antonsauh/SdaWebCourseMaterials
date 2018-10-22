4bc509cf7a007df071e6b68f498cffb2022f2103
Anton Sauh <AntonSauh@Antons-MacBook-Pro.local>
2018-10-22 20:35:27 +0300

    implement

--- EX_01/script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    document.getElementById('my-id').style.color = "black";
-    document.getElementById('my-id').setAttribute('style', 'color: pink');
-    document.getElementById('my-id').addEventListener('click', changeColor);
-};
-function changeColor() {
-    document.getElementById('my-id').style.color = "black";
-}
