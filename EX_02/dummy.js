8a317580e9323469138b8a4f3bb0c94c84658b15
Anton Sauh <AntonSauh@Antons-MacBook-Pro.local>
2018-10-23 19:50:21 +0300

    add dummy.js

---
+++ EX_02/dummy.js
@@ -0,0 +1,84 @@
+// function doOnClick() {
+//     console.log("button has been pressed");
+// }
+// $(document).ready(function () {
+//     console.log('ready');
+//     $('#about-me').click(doOnClick);
+// });
+
+
+window.onload = (() => {
+    // let x = function (someValue, callback) {
+    //     console.log(someValue);
+    //     callback();
+    // };
+    //
+    // x("valuePassed", function () {
+    //     console.log("Here is Callback");
+    // });
+
+    // let myList = [1, 2, 3, 4];
+    // let myAnotherList = ['c', 'h', 'b'];
+    // let someOtherList = [myAnotherList, myList];
+    //
+    // console.log(someOtherList);
+
+    // for(let i = 0; i < myAnotherList.length; i++) {
+    //     console.log(myAnotherList[i]);
+    // }
+
+    // //passing an big arrow function
+    // myAnotherList.forEach((value) => {
+    //     console.log(value);
+    // });
+    // // passing an anonymous function
+    // myAnotherList.forEach(function(value){
+    //     console.log(value);
+    // });
+    // // creating a function
+    // let x = function(value) {
+    //     console.log(value)
+    // };
+    // // passing the created function
+    // myAnotherList.forEach(x);
+    // let i = 0;
+    // while(i < 10) {
+    //     console.log(i);
+    //     i++;
+    // }
+    // let i = 0;
+    //
+    // do {
+    //
+    //     console.log(i);
+    //     i++;
+    //
+    // } while(i < 0);
+    // let i = 0;
+    //
+    // if (i === 0) {
+    //
+    // }
+    // else if (i > 0) {
+    //
+    // } else {
+    //
+    // }
+
+
+
+
+});
+
+
+// window.onload = function () {
+//
+//     let x = function (someValue, callback) {
+//         console.log(someValue);
+//         callback();
+//     };
+//
+//     x("valuePassed", function () {
+//         console.log("Here is Callback");
+//     });
+// };
