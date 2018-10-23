f38362e0efe09b30bed5b15c5e139c662475098f
Anton Sauh <AntonSauh@Antons-MacBook-Pro.local>
2018-10-23 21:20:54 +0300

    23.10.2018

---
+++ EX_02/script.js
@@ -0,0 +1,16 @@
+
+
+window.onload = () => {
+    document.getElementById("about-me").addEventListener('click', myFunction)
+};
+
+function myFunction() {
+    window.open('about_me.html', '_self');
+}
+
+// let myFunction = function () {
+//
+// }
+function goBackToMainPage() {
+    window.open('index.html', '_self');
+}
